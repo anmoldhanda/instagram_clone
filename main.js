51bc8cd5f4632b9ba839e204830089f737900515
anmol dhanda <rk352714@gmail.com>
2024-02-11 23:50:52 +0530

    show hide suggested user profile details cards as the user hovers on the profile username title

--- main.js
+++ main.js
@@ -6,3 +6,52 @@ const moreoptionspopupbtn = document.querySelector(".moreoptionspopupbtn");
 moreoptionspopupbtn.addEventListener("click", () => {
   moreoptionspopupcontainer.classList.toggle("openclose");
 });
+
+//hide show the user's profile details when the user hover's over the specific suggested user's profile name
+const firstprofileusername = document.getElementById("firstprofileusername");
+const secondprofileusername = document.getElementById("secondprofileusername");
+const thirdprofileusername = document.getElementById("thirdprofileusername");
+const fourthprofileusername = document.getElementById("fourthprofileusername");
+const firstprofileusernamedetails = document.getElementById(
+  "firstprofileusernamedetails"
+);
+const secondprofileusernamedetails = document.getElementById(
+  "secondprofileusernamedetails"
+);
+const thirdprofileusernamedetails = document.getElementById(
+  "thirdprofileusernamedetails"
+);
+const fourthprofileusernamedetails = document.getElementById(
+  "fourthprofileusernamedetails"
+);
+firstprofileusername.addEventListener("mouseover", () => {
+  firstprofileusernamedetails.classList.add("showhide");
+});
+
+firstprofileusername.addEventListener("mouseout", () => {
+  firstprofileusernamedetails.classList.remove("showhide");
+});
+
+secondprofileusername.addEventListener("mouseover", () => {
+  secondprofileusernamedetails.classList.add("showhide");
+});
+
+secondprofileusername.addEventListener("mouseout", () => {
+  secondprofileusernamedetails.classList.remove("showhide");
+});
+
+thirdprofileusername.addEventListener("mouseover", () => {
+  thirdprofileusernamedetails.classList.add("showhide");
+});
+
+thirdprofileusername.addEventListener("mouseout", () => {
+  thirdprofileusernamedetails.classList.remove("showhide");
+});
+
+fourthprofileusername.addEventListener("mouseover", () => {
+  fourthprofileusernamedetails.classList.add("showhide");
+});
+
+fourthprofileusername.addEventListener("mouseout", () => {
+  fourthprofileusernamedetails.classList.remove("showhide");
+});
